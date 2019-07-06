54ac77abad71ea91f53ee99080c38a4fe743d47f
davidh <iq.davidh@gmail.com>
2019-07-06 14:39:21 -0500

    ya trabajando en la lista

--- front/src/components/Notificaciones.vue
+++ front/src/components/Notificaciones.vue
@@ -1,8 +1,26 @@
 <template>
 
     <div class="notificaciones">
+
+        <div style="text-align: center">
+            <img src="/logo.png" title="Logotipo"/>
+            <input type="text" style="float: right" v-model="textoBuscado">
+        </div>
+
         <h3>Listado de notificaciones</h3>
-       
+
+
+        <div>
+
+            <div style="display: flex" class="headerCointainer">
+                <div>Mis Preferencias</div>
+                <div>Ver Todas</div>
+            </div>
+            <div v-for="item in listaNFiltro" v-bind:key="item.id" class="itemTablaN">
+
+            </div>
+        </div>
+
     </div>
 
 
@@ -10,39 +28,44 @@
 
 <script>
 
-    export default {
-        name: "Notificaciones",
-        data() {
-            return {
-                notificaciones: [],
-                notificacionesConFiltro:[],
-                textoBuscado:''
-            }
-        },
-        methods:{
+	export default {
+		name: "Notificaciones",
+		data() {
+			return {
+				listaN: [],
+				listaNFiltro: [],
+				textoBuscado: ''
+			}
+		},
+		methods: {},
+		mounted() {
 
-        },
-        mounted(){
-
-            //
-            // fetch('http://localhost:3000/movies')
-            //     .then((response)=>{
-            //         return response.json()
-            //     })
-            //     .then((json) =>{
-            //         this.notificaciones =json;
-            //     })
-            //     .catch(error=>{
-            //         alert(error);
-            //     })
+			//
+			// fetch('http://localhost:3000/movies')
+			//     .then((response)=>{
+			//         return response.json()
+			//     })
+			//     .then((json) =>{
+			//         this.notificaciones =json;
+			//     })
+			//     .catch(error=>{
+			//         alert(error);
+			//     })
 
 
-        }
-    }
+		}
+	}
 
 </script>
 
 <style scoped>
 
+    .headerCointainer {
+        display: flex; text-align: center;
+    }
+
+    .itemTablaN{
+        display: ;
+    }
 
 </style>
