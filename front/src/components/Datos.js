0ad8876da14513d21db9a22afc6eb1f35e191c42
Jarsory <arsory92@gmail.com>
2019-07-06 15:23:12 -0500

    notificaciones

--- front/src/components/Datos.js
+++ front/src/components/Datos.js
@@ -5,5 +5,71 @@
  */
 
 let listaDesastre=[
-	{}
+	{
+	tipo: "Sismo",
+	descripcion: "Magnitud 8.2, con epicentro en el sur de México. Estados afectados: Oaxaca, Chiapas y ciudad de México",
+	fechainicio:"07 de septiembre de 2017",
+	Hora: "23:49",
+	ubicacion:"Juchitán, Oaxaca",
+	niveldegravedad:"Alta",
+	radiodeafectacion: "230 Km",
+	id: "1"
+	},
+
+	{
+	tipo: "Inundación",
+	descripcion: "Fuertes lluvias en Campeche, afectan calles y avenidas pricipales",
+	fechainicio:"31 mayo de 2019",
+	ubicacion:"Campeche, Campeche",
+	niveldegravedad:"Alta",
+	id: "2"
+	},
+
+	{
+	tipo: "Huracán",
+	descripcion: "Huracán Katia. Estados afectados Campeche y Veracruz",
+	fechainicio:"05 de septiembre de 2017",
+	fechafin:"09 de septiembre de 2017",
+	ubicacion:"Campeche",
+	niveldegravedad:"Alta",
+	id: "3"
+	},
+
+	{
+	tipo: "Inundación",
+	descripcion: "Fuerte tormenta ocasiona una inundación en la Avenida López Portillo, en los carriles con dirección a Ecatepec, en Coacalco, Estado de México.",
+	fechainicio:"01 julio de 2019",
+	ubicacion:"Coacalco, Estado de México",
+	niveldegravedad:"Moderado",
+	id: "4"
+	},
+
+	{
+	tipo: "Erupción volcánica",
+	descripcion: "Violenta erupción del volcán Stromboli en Italia",
+	fechainicio:"03 de julio de 2019",
+	hora:"14:44",
+	ubicacion:"Italia",
+	niveldegravedad:"Alta",
+	radiodeafectacion: "10 Km",
+	id: "5"
+	},
+
+	{
+	tipo: "Incendio",
+	descripcion: "Incendio boscoso de grandes proporciones en norte de Italia",
+	fechainicio:"25 de marzo de 2019",
+	fechafin:"26 de marzo de 2019",
+	ubicacion:"Italia",
+	niveldegravedad:"Alto",
+	radiodeafectacion: "90 Km",
+	id: "6"
+	},
+
+	
+
+
+
 ];
+
+	
